c7d802867e94698ef4613109fd06c12fa07f4913
Renato Silva <reeh.cr2@gmail.com>
2021-04-01 19:44:54 -0300

    Add sorted e group

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val joao = Funcionario("joao",1000.0)
-    val maria = Funcionario("maria",2000.0)
-    val jose = Funcionario("jose",800.0)
+    val joao = Funcionario("joao",1000.0,"CLT")
+    val maria = Funcionario("maria",2000.0,"CLT")
+    val jose = Funcionario("jose",800.0,"PJ")
 
     val funcionarios = listOf(joao, maria, jose)
 
@@ -16,12 +16,27 @@ fun main() {
         it.nome == "maria"
     })
 
+    println("===========================")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach{
+            println(it)
+        }
+
+    println("===========================")
+    funcionarios
+        .groupBy{ it.tipoContratacao }
+        .forEach{
+            println(it)
+        }
+
 
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ){
     override fun toString(): String =
         """
